566f4a8bf385e1c0e279819a4c5142fb69627b87
makeza86 <maxbarin86@gmail.com>
2026-05-11 05:51:56 +0500

    Update MainActivity.java

--- app/src/main/java/com/jimongroup/jimongramm/MainActivity.java
+++ app/src/main/java/com/jimongroup/jimongramm/MainActivity.java
@@ -69,7 +69,6 @@ public class MainActivity extends Activity {
             }
 
             @Override
-            @Override
 public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
     handler.cancel();
             }
